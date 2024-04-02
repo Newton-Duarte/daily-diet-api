54a4eb9e8c0d83bea4b0e671bb45498af85dadc5
Newton Duarte <newton_@hotmail.com>
2024-04-02 10:26:16 -0300

    chore: add create meals migration

---
+++ db/migrations/20240402132157_create-meals.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description')
+    table.boolean('inside_diet').defaultTo(false)
+    table.timestamps()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
