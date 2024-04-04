509f22214bf2bcb872d24956a6265c02e7222170
Newton Duarte <newton_@hotmail.com>
2024-04-04 11:21:40 -0300

    chore: fix date column migration

--- db/migrations/20240402135736_add-date-to-meals.ts
+++ db/migrations/20240402135736_add-date-to-meals.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('meals', (table) => {
-    table.dateTime('date')
+    table.datetime('date')
   })
 }
 
