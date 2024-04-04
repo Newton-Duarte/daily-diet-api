56d962bd6b4b72069de554456960c1e16a02204a
Newton Duarte <newton_@hotmail.com>
2024-04-04 11:45:25 -0300

    fix: user id references on meals table

--- db/migrations/20240402134500_add-user-id-to-meals.ts
+++ db/migrations/20240402134500_add-user-id-to-meals.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('meals', (table) => {
-    table.uuid('user_id').after('id').index()
+    table.uuid('user_id').references('users.id').notNullable().index()
   })
 }
 
